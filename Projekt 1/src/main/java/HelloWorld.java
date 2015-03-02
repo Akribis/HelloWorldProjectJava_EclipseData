826e39a069f21f1cc5fb34d4dbf5dcad2c2b2a66
mpruscha <pruscha.martin@gmail.com>
2015-03-03 00:08:54 +0100

    Trying to use netbeans for "team" commit

--- Projekt 1/src/main/java/HelloWorld.java
+++ Projekt 1/src/main/java/HelloWorld.java
@@ -1,9 +1,24 @@
 
+
+import java.util.Scanner;
+
+
+
 public class HelloWorld {
 
+    
 	public static void main(String[] args) {
 
-		System.out.println("Hello Akribis!!!!");
+                Scanner user_input = new Scanner( System.in );
+            
+		System.out.println("Hello Akribis!!!!\n");
+                
+                String first_name;
+                System.out.print("Enter Akribis' name\n");
+                
+                first_name = user_input.next();
+                
+                System.out.println("You are " + first_name);
 		
 	}
 
