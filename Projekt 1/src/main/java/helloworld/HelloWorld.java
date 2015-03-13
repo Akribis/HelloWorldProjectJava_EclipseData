e612368677df7d84b7d41b586a19e528e3ff72ee
mpruscha <pruscha.martin@gmail.com>
2015-03-13 19:50:04 +0100

    integrated LWJGL. whole program needs to be build by executing mvn package in the Projekt 1 folder. To run, extract the created .zip file in the target folder.

--- Projekt 1/src/main/java/helloworld/HelloWorld.java
+++ Projekt 1/src/main/java/helloworld/HelloWorld.java
@@ -1,39 +1,71 @@
 package helloworld;
 
-
+import java.lang.reflect.Field;
 import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
 
 public class HelloWorld {
-    public void start() {
+
+    public static void main(String[] args) {
+        
+        // fix from http://forum.lwjgl.org/index.php?topic=5322.0
+        System.setProperty("java.library.path", "natives");
         try {
-            Display.setDisplayMode(new DisplayMode(800,600));
+            final Field sysPathsField = ClassLoader.class.getDeclaredField("sys_paths");
+            sysPathsField.setAccessible(true);
+            try {
+                sysPathsField.set(null, null);
+            } catch (IllegalAccessException e) {
+
+            }
+        } catch (NoSuchFieldException e) {
+
+        }
+
+        int targetWidth = 640;
+        int targetHeight = 480;
+
+        try {
+            DisplayMode chosenMode = new DisplayMode(targetWidth, targetHeight);
+
+            Display.setDisplayMode(chosenMode);
+            Display.setTitle("Example Maven Natives");
+            Display.setFullscreen(false);
             Display.create();
         } catch (LWJGLException e) {
-            e.printStackTrace();
+            Sys.alert("Error", "Unable to create display.");
             System.exit(0);
         }
-         
-        // init OpenG here
-         
-        while (!Display.isCloseRequested()) {
-             
-            // render OpenGL here
-             
+
+        GL11.glClearColor(0, 0, 0, 0);
+
+        boolean gameRunning = true;
+        float pos = 0;
+
+        int FRAMERATE = 60;
+
+        while (gameRunning) {
+            GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_STENCIL_BUFFER_BIT);
+
+            GL11.glRotatef(0.6f, 0, 0, 1);
+            GL11.glBegin(GL11.GL_TRIANGLES);
+            GL11.glVertex3f(-0.5f, -0.5f, 0);
+            GL11.glVertex3f(0.5f, -0.5f, 0);
+            GL11.glVertex3f(0, 0.5f, 0);
+
+            GL11.glEnd();
+
             Display.update();
+            Display.sync(FRAMERATE);
+
+            if (Display.isCloseRequested()) {
+                gameRunning = false;
+                Display.destroy();
+                System.exit(0);
+            }
         }
-         
-        Display.destroy();
     }
-     
-    public static void main(String[] argv) {
-        
-        System.out.println("hello display-akribis!!!");
-
-        HelloWorld displayExample = new HelloWorld();
-        displayExample.start();
-    }
-    
-
 }
